5e61ca51b4f7d9e657f4335d099d6a2213b975c3
angurgapi <sophiekovalsky@gmail.com>
2021-01-13 14:31:48 +0300

    added mika-nails

--- scrpt.js
+++ scrpt.js
@@ -1,11 +1,8 @@
 let tiles = document.querySelectorAll('.project-tile');
 console.log(tiles);
-
-
 tiles.forEach(tile => tile.addEventListener("mouseover", function() {   
    tile.style.transform = "scale(1.1)";
    tile.style.opacity = '1';
-
 }));
 
 tiles.forEach(tile => tile.addEventListener("mouseout", function() {   
